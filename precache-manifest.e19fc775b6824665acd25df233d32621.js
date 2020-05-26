092bcd1e8cb494e25ee64fcb0aac42697afddc26
Peter <39281847+ple-dev@users.noreply.github.com>
2020-05-26 15:42:30 -0600

    Updates

---
+++ precache-manifest.e19fc775b6824665acd25df233d32621.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eccec0a21a68646cc53cf03e8f7bea12",
+    "url": "/githubtest/index.html"
+  },
+  {
+    "revision": "d4b063cc8d7ca23390a2",
+    "url": "/githubtest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "514e4e7444fcda61ccbc",
+    "url": "/githubtest/static/js/2.81ac20ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/githubtest/static/js/2.81ac20ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4b063cc8d7ca23390a2",
+    "url": "/githubtest/static/js/main.d34db850.chunk.js"
+  },
+  {
+    "revision": "8a96afa7e26897b7b393",
+    "url": "/githubtest/static/js/runtime-main.097065d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/githubtest/static/media/logo.5d5d9eef.svg"
+  }
+]);
